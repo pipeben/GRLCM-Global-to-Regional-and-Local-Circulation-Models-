41510909e8e9333a21e2b44af4129a8e1c8aca4b
pipeben <97968531+pipeben@users.noreply.github.com>
2023-03-11 06:26:03 -0500

    Update Bathymetric_Projection_Method.R

--- Bathymetric_Projection_Method.R
+++ Bathymetric_Projection_Method.R
@@ -1,4 +1,4 @@
-https://github.com/pipeben/Bathymetric-Projection
+# https://github.com/pipeben/Bathymetric-Projection
 # This R-script provides the codes and explanation of GIS tools needed to 
 # perform Bathymetric Projection (BP) on environmental ocean variables measured 
 # as vertical layers throughout the depth profile (depth dimension) of the ocean
